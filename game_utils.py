95534ffeb01633edf9a36f23109e0141e9c2781e
Arrobert2202 <robertaron15@yahoo.ro>
2023-12-31 00:49:37 +0200

    improved gui

--- game_utils.py
+++ game_utils.py
@@ -1,14 +1,17 @@
 import tkinter as tk
 from tkinter import ttk
+import random
 
-MAXHEIGHT = 18
+MAXHEIGHT = 14
 MAXWIDTH = 12
+BUBBLESIZE = 38
 
 class Game:
   def __init__(self, levels_data):
     self.window = tk.Tk()
     self.table_rows = 0
     self.levels_data = levels_data
+    self.all_colors = set()
     self.configure_window()
     self.window.mainloop()
 
@@ -44,6 +47,9 @@ class Game:
     self.game_gui()
     self.create_level_table(current_level)
 
+    #self.draw_current_bubble()
+    #self.get_next_bubble()
+
   def game_gui(self):
     self.destroy_widgets()
 
@@ -53,14 +59,19 @@ class Game:
     level_label = tk.Label(top_frame, text="Level 1", font=('Arial', 12, 'bold'), bg='#7700a6', fg='#defe47')
     level_label.pack(pady=5)
 
-    next_bubble_label = tk.Label(top_frame, text="Next bubble: ", font=('Arial', 12, 'bold'), bg='#7700a6', fg='#defe47')
-    next_bubble_label.pack(pady=5)
+    self.next_bubble_label = tk.Label(top_frame, text="Next bubble: ", font=('Arial', 12, 'bold'), bg='#7700a6', fg='#defe47')
+    self.next_bubble_label.pack(side=tk.LEFT, pady=5)
+
+    self.next_bubble_canvas = tk.Canvas(top_frame, width = 20, height = 20, bg='#7700a6', highlightthickness=0)
+    self.next_bubble_canvas.pack(side = tk.LEFT)
 
     menu_button = tk.Button(top_frame, text='Go to menu', foreground='#7700a6', background='#defe47', command=lambda: self.menu_components())
     menu_button.place(relx=0.97, rely=0.25, anchor=tk.NE)
 
-    self.game_canvas = tk.Canvas(self.window, width=450, height=550, bg='#092067', highlightbackground='#fe00fe', highlightthickness=10)
+    self.game_canvas = tk.Canvas(self.window, width=456, height=550, bg='#092067', highlightbackground='#fe00fe', highlightthickness=1)
     self.game_canvas.pack(pady=10)
+    line_y = (MAXHEIGHT - 2) * BUBBLESIZE + BUBBLESIZE // 2
+    self.game_canvas.create_line(70, line_y, 386, line_y, width = 3, fill = '#7700a6')
 
     score_label = tk.Label(self.window, text="Score: ", font=('Arial', 16, 'bold'), bg='#7700a6', fg='#defe47')
     score_label.pack()
@@ -73,23 +84,45 @@ class Game:
     bubble_matrix = [[None for _ in range(MAXWIDTH)] for _ in range(MAXHEIGHT)]
     return bubble_matrix
 
-  def populate_table(self, game_table, bubbles_data):
+  def populate_table(self, bubbles_data):
     for bubble in bubbles_data:
-      game_table[bubble['row']][bubble['col']] = Bubble(bubble['color'], bubble['row'], bubble['col'])
-
-    return game_table
+      self.all_colors.add(bubble['color'])
+      self.game_table[bubble['row']][bubble['col']] = Bubble(bubble['color'], bubble['row'], bubble['col'])
+    print(self.all_colors)
+    return self.game_table
 
   def create_level_table(self, current_level):
     level_data = self.levels_data['levels'][current_level - 1]
     bubbles_data = level_data['bubbles']
 
-    game_table = self.initial_matrix()
-    self.populate_table(game_table, bubbles_data)
+    self.game_table = self.initial_matrix()
+    self.populate_table(bubbles_data)
 
-    for row in range(len(game_table)):
-      for col in range(len(game_table[0])):
-        if game_table[row][col] is not None:
-          game_table[row][col].draw_bubble(self.window, self.game_canvas)
+    for row in range(len(self.game_table)):
+      for col in range(len(self.game_table[0])):
+        if self.game_table[row][col] is not None:
+          self.game_table[row][col].draw_bubble(self.game_canvas)
+
+    self.current_color = self.random_bubble()
+    self.draw_current_bubble()
+    self.next_bubble_color = self.random_bubble()
+    self.draw_next_bubble()
+    
+  def get_next_bubble(self):
+    self.current_color =  self.next_bubble_color
+    self.next_bubble_color = self.random_bubble()
+    self.draw_next_bubble()
+  
+  def draw_next_bubble(self):
+    self.next_bubble_canvas.create_oval(0, 0, 20, 20, fill = self.next_bubble_color)
+
+  def draw_current_bubble(self):
+    print("current")
+    self.current_bubble = Bubble(self.current_color, 15, 5)
+    self.current_bubble.draw_bubble(self.game_canvas)
+
+  def random_bubble(self):
+    return random.choice(list(self.all_colors))
 
 class Bubble:
   def __init__(self, color, row, col):
@@ -98,4 +131,13 @@ class Bubble:
     self.col = col
 
   def draw_bubble(self, game_canvas):
-    print("canvas")
+    x, y = bubble_positions(self.row, self.col)
+    bubble_radius = BUBBLESIZE // 2
+    game_canvas.create_oval(x - bubble_radius, y - bubble_radius, x + bubble_radius, y + bubble_radius, fill = self.color)
+
+def bubble_positions(row, col):
+  x = BUBBLESIZE * col + BUBBLESIZE // 2
+  if row % 2 == 1:
+    x += BUBBLESIZE // 2
+  y = BUBBLESIZE * row * 0.86 + BUBBLESIZE // 2
+  return x, y
